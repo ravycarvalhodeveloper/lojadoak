aa7f4ab3161e00803182d738e27b6159d0ff6fe5
Ravy Carvalho <164415227+ravycarvalhodeveloper@users.noreply.github.com>
2024-10-11 10:31:09 -0300

    Update index.tsx

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function Container({children}: {children: ReactNode}) {
     return (
-        <div className="w-full scale-90 px-4 flex flex-col justify-center items-center">
+        <div className="w-full scale-90 px-5 flex flex-col justify-center items-center">
             {children}
         </div>
     )
